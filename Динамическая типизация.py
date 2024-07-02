b6a2382854a572db18d4ac7d0c353d04f4372830
Gritz7 <makarevichsa77@gmail.com>
2024-07-02 13:14:15 +0300

    Upd Динамическая типизация.py

--- Динамическая типизация.py
+++ Динамическая типизация.py
@@ -2,7 +2,7 @@ name = "Sergei"
 print(f"Name:{name}")
 age = 22
 print(f"Age:{age}")
-new_age = age + 1
-print(f"New age:{new_age}")
+age = age + 1
+print(f"New age:{age}")
 is_student = True
 print(f"Student:{is_student}")
