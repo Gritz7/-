6ce834bd103ae05b11fe8446da4fe523ce9a5af3
Gritz7 <makarevichsa77@gmail.com>
2024-12-09 17:33:26 +0300

    Add files via upload

---
+++ тест.py
@@ -0,0 +1,21 @@
+# 1st program
+print(9**0.5 * 5)
+
+# 2nd program
+
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# 3rd program
+
+result1 = (2 * 2 + 2)
+result2 = (2 * (2 + 2))
+print(result1 == result2)
+
+#4th program
+
+first_str = '123.456'
+first_float = float(first_str)
+sdvig = first_float * 10
+remove_float = int(sdvig)
+digit = remove_float % 10
+print(digit)
