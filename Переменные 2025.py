f9c8bdca31705c027596eb9769f262d72d026ca8
Gritz7 <makarevichsa77@gmail.com>
2025-01-15 15:56:11 +0300

    Add files via upload

---
+++ Переменные 2025.py
@@ -0,0 +1,6 @@
+homework_quantity = 12
+hours_spent = 1.5
+course_name = "Python"
+hours_spent_per_task =round(hours_spent / homework_quantity, 2)
+print(f"Курс: {course_name}, всего задач: {homework_quantity}, затрачено часов: {hours_spent}, среднее время на одно задание: {hours_spent_per_task}")
+
