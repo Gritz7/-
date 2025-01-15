ec5f82568712390073756ea6506822b40a5e0b45
Gritz7 <makarevichsa77@gmail.com>
2025-01-15 20:14:36 +0300

    Add files via upload

---
+++ Организация программ и методы строк 2025.py
@@ -0,0 +1,10 @@
+name = input("Введите имя: ")
+length = len(name.replace(" ",""))
+print(f"Количество букв в вашем имени: {length}")
+
+print(name.upper())
+print(name.lower())
+print(name.replace(" ",""))
+print(name[0])
+print(name[-1])
+
